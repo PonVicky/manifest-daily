eae9a956b40a725bc0c1a2340e0a2b6c4b4eea07
PonVignesh <ponvignesh6@gmail.com>
2026-07-01 03:01:27 +0530

    Added posthog cofig

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import posthog from 'posthog-js'
+
+posthog.init('phc_kJXDidAJviSTthKevJfELwv6pnqHcJ2TQL6pLFuRoGS3', {
+  api_host: 'https://us.i.posthog.com'
+})
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
